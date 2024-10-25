23be33c7505488a174e09d4c7d27315551a9c080
melinamelo <melinamelo0688@gmail.com>
2024-10-25 13:56:01 -0300

    teste

--- Repositorios/PostagemRepositorio.cs
+++ Repositorios/PostagemRepositorio.cs
@@ -14,7 +14,6 @@ namespace Api.Repositorios
         {
             _dbContext = dbContext;
         }
-
         public async Task<List<PostagemCompleta>> GetAll()
         {
             var postagens = await _dbContext.Postagem
